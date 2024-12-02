debd46d6d58532a46426c8821f24b16aca728471
Ethan Harmon <ethan.harmon@clearwatersavings.com>
2024-12-02 13:59:27 -0500

    Finished day1

--- day1/solution.go
+++ day1/solution.go
@@ -21,8 +21,9 @@ func main() {
 
 	sum := 0
 
-	sum += part1(scanner)
-	// fmt.Println(part2())
+	// sum += part1(scanner)
+	sum += part2(scanner)
+	fmt.Println(sum)
 }
 
 func part1(scanner *bufio.Scanner) int {
@@ -35,7 +36,65 @@ func part1(scanner *bufio.Scanner) int {
 		right = append(right, right_num)
 	}
 
-	return 0
+	mergeSort(left, 0, len(left)-1)
+	mergeSort(right, 0, len(right)-1)
+
+	sum := 0
+
+	for i := range len(left) {
+		sum += int(math.Abs(float64(left[i] - right[i])))
+	}
+
+	return sum
+}
+
+func part2(scanner *bufio.Scanner) int {
+	left, right := []int{}, []int{}
+	for scanner.Scan() {
+		numbers := strings.Split(scanner.Text(), "   ")
+		left_num, _ := strconv.Atoi(numbers[0])
+		right_num, _ := strconv.Atoi(numbers[1])
+		left = append(left, left_num)
+		right = append(right, right_num)
+	}
+
+	mergeSort(left, 0, len(left)-1)
+	mergeSort(right, 0, len(right)-1)
+
+	sum := 0
+
+	for i := range len(left) {
+		right_num_greater := false
+		similarity_score := 0
+		iter := 0
+		for right_num_greater == false && iter < len(left) {
+			fmt.Println("Left:", left[i])
+			fmt.Println("Right:", right[iter])
+			if left[i] > right[iter] {
+				fmt.Println("Right is smaller")
+				fmt.Println()
+				iter++
+				continue
+			} else if left[i] == right[iter] {
+				similarity_score++
+				iter++
+				fmt.Println("Incrementing similarity score to", similarity_score)
+			} else {
+				right_num_greater = true
+				fmt.Println("Right is greater, breaking")
+			}
+			fmt.Println()
+		}
+		sum += left[i] * similarity_score
+		fmt.Printf("Similarity score for %d is %d\n", left[i], left[i]*similarity_score)
+		fmt.Printf("Running sum is %d", sum)
+		fmt.Println()
+		fmt.Println("------------------------------------------")
+		fmt.Println()
+
+	}
+
+	return sum
 }
 
 // TODO: Finish mergeSort
@@ -45,6 +104,9 @@ func mergeSort(array []int, left, right int) {
 	}
 
 	mid := int(math.Floor(float64(left+right) / 2))
+	mergeSort(array, left, mid)
+	mergeSort(array, mid+1, right)
+	merge(array, left, mid, right)
 }
 
 func merge(array []int, left, mid, right int) {
