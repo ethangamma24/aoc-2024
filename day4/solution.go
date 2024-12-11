11b4ad65fb714d621b203411dbecb6b9541b5d47
Ethan Harmon <ethanharmon2018@gmail.com>
2024-12-11 14:08:17 -0500

    Finished day 4 part 1

--- day4/solution.go
+++ day4/solution.go
@@ -30,50 +30,71 @@ func main() {
 		return
 	}
 
-	part1()
-	// part2()
+	XMAS_FOUND += part1()
+	// XMAS_FOUND += part2()
 	fmt.Println(XMAS_FOUND)
 }
 
-func part1() {
-	// Starting at 1st index bc we're searching for 'X' at surface level
+func part1() int {
+	sum := 0
+	// Starting at 2nd index bc we're searching for 'X' at surface level, then
+	// when we find an 'M' around it, we'll be searching in a straight line
 	xmas_index := 1
 
 	for y, row := range XMAS_MAP {
 		for x, char := range row {
+			fmt.Printf("Looking for %c at [%d][%d]\n", 'X', y+1, x+1)
 			if char == 'X' {
-				fmt.Printf("Found %c, searching for %c\n", 'X', XMAS[xmas_index])
-				searchRecursively(x, y, xmas_index)
-			}
-		}
-	}
-}
-
-func part2() {
-
-}
-
-func searchRecursively(x, y, index int) {
-	for i := x - 1; i <= x+1; i++ {
-		for j := y - 1; j <= y+1; j++ {
-			// fmt.Printf("Looking at [%d][%d]\n", j, i)
-			char, err := getAtIndex(i, j)
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				if char == XMAS[index] {
-					// fmt.Printf("Checking %c against %c\n", char, XMAS[index])
-					if char == 'S' {
-						fmt.Println("Found S, incrementing XMAS_FOUND")
-						XMAS_FOUND++
-						return
-					} else {
-						fmt.Printf("Found %c, searching for %c\n", XMAS[index], XMAS[index+1])
-						searchRecursively(i, j, index+1)
+				fmt.Printf("Found %c at [%d][%d], searching for %c\n", 'X', y+1, x+1, XMAS[xmas_index])
+				// searchRecursively(x, y, xmas_index)
+				for i := x - 1; i <= x+1; i++ {
+					for j := y - 1; j <= y+1; j++ {
+						char, err := getAtIndex(i, j)
+						fmt.Printf("Looking at [%d][%d]: %c\n", j+1, i+1, char)
+						if err != nil {
+							fmt.Println(err)
+						} else {
+							// If we find M, continue searching in that "line"
+							// for the last 2 characters
+							// fmt.Printf("Found %c at [%d][%d], searching for %c\n", XMAS[xmas_index], j+1, i+1, XMAS[xmas_index+1])
+							if char == 'M' {
+								fmt.Printf("Found %c at [%d][%d], searching for %c\n", XMAS[xmas_index], j+1, i+1, XMAS[xmas_index+1])
+								sum += searchStraightLine(i, j, i-x, j-y, xmas_index+1)
+							}
+						}
 					}
 				}
 			}
 		}
+	}
+	return sum
+}
+
+func part2() int {
+	return 0
+}
+
+func searchStraightLine(x, y, dir_x, dir_y, index int) int {
+	new_x, new_y := x+dir_x, y+dir_y
+	fmt.Printf("Direction: %d, %d\n", dir_y, dir_x)
+	fmt.Printf("Looking at [%d][%d]\n", new_y+1, new_x+1)
+	char, err := getAtIndex(new_x, new_y)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+	if char == XMAS[index] {
+		if char == 'S' {
+			fmt.Println("Found S, incrementing XMAS_FOUND")
+			fmt.Println("--------------------------------")
+			return 1
+		} else {
+			fmt.Printf("Found %c at [%d][%d], searching for %c\n", XMAS[index], new_y+1, new_x+1, XMAS[index+1])
+			return searchStraightLine(new_x, new_y, dir_x, dir_y, index+1)
+		}
+	} else {
+		fmt.Printf("Found %c, exiting\n", char)
+		return 0
 	}
 }
 
