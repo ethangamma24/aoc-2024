e9b6336a5eb033d5b87d88608c81c8968a5fc5a1
Ethan Harmon <ethan.harmon@clearwatersavings.com>
2024-12-18 16:20:24 -0500

    Set up day 6 part 1

--- day6/solution.go
+++ day6/solution.go
@@ -6,6 +6,13 @@ import (
 	"os"
 )
 
+type COORDS struct {
+	x int
+	y int
+}
+
+var MAP [][]rune
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -16,6 +23,14 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	MAP = createMap(scanner)
+	DIRECTION := map[string]COORDS{
+		"UP": {x: 0, y: -1},
+		"RIGHT": {x: 1, y: 0},
+		"DOWN": {x: 0, y: 1},
+		"LEFT": {x: -1, y: 0},
+	}
+
 	sum := 0
 
 	if err := scanner.Err(); err != nil {
@@ -23,15 +38,42 @@ func main() {
 		return
 	}
 
-	sum += part1(scanner)
-	// sum += part2(scanner.Text())
+	var starting_position COORDS
+	starting_position_found := false
+	// Find guard's starting location
+	for i := 0; i < len(MAP); i++ {
+		for j := 0; j < len(MAP[i]); j++ {
+			if starting_position_found {
+				break
+			}
+			if MAP[i][j] == '^' {
+				starting_position := COORDS{j,i}
+				starting_position_found = true
+			}
+		}
+		if starting_position_found {
+			break
+		}
+	}
+
+	sum += part1(starting_position)
+	// sum += part2(starting_position)
 	fmt.Println(sum)
 }
 
-func part1(scanner *bufio.Scanner) int {
+func part1(position COORDS) int {
+	
+}
+
+func part2(position COORDS) int {
 
 }
 
-func part2(scanner *bufio.Scanner) int {
-
+func createMap(scanner *bufio.Scanner) [][]rune {
+	map := make([][]rune, 0)
+	for scanner.Scan() {
+		line := []rune(scanner.Text())
+		map = append(map, line)
+	}
+	return map
 }
